391d591259c47126f5cb8d1950aa1e80b49d4f0a
Andres <andres-gaudioso@outlook.es>
2021-07-11 12:10:54 +0200

    Eliminado el superheroe

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # smartlife
 Probando a capturar las anotaciones
+Browser en
+http://localhost:8888/explorer/index.html#uri=/
--- src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
-
--- Brisbane
-insert into superhero(id, name, description) values (0,'Superlopez', 'Supper Heroe Nacional');
-insert into superhero(id, name, description) values (1,'Superman', 'Superheroe con calzones');
-insert into superhero(id, name, description) values (2,'Spiderman', 'Super heroe en pijama');
-
-
